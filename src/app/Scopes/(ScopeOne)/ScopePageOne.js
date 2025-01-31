7f008a2a096b0c2346971097f8d40aa5110dbb98
vmkmanokumar <intern1@veyyil.com>
2025-01-31 14:39:22 +0530

    flex changes

--- src/app/Scopes/(ScopeOne)/ScopePageOne.js
+++ src/app/Scopes/(ScopeOne)/ScopePageOne.js
@@ -30,22 +30,22 @@ export default function ScopePageOne() {
       {/* Scope Section */}
       <Flex
         justify="center"
-        className="sm:mt-[100px] md:mt-[150px] text-[18px] sm:text-[24px]"
+        className="sm:mt-[100px] md:mt-[100px] text-[18px] sm:text-[24px]"
         gap={20}
         style={{ padding: "0 20px" }}
       >
         <Row gutter={[16, 16]} justify="center" style={{ width: "100%", maxWidth: "1200px" }}>
-          <Col xs={24} sm={8} md={8} lg={8} xl={8}>
+          <Col xs={8} sm={8} md={8} lg={8} xl={8}>
             <div style={{ textAlign: "center" }}>
               <h1>Scope 1</h1>
             </div>
           </Col>
-          <Col xs={24} sm={8} md={8} lg={8} xl={8}>
+          <Col xs={8} sm={8} md={8} lg={8} xl={8}>
             <div style={{ textAlign: "center" }}>
               <h1>Scope 2</h1>
             </div>
           </Col>
-          <Col xs={24} sm={8} md={8} lg={8} xl={8}>
+          <Col xs={8} sm={8} md={8} lg={8} xl={8}>
             <div style={{ textAlign: "center" }}>
               <h1>Scope 3</h1>
             </div>
