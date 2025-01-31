7af135fe49cef5dd84294a00c25f2a6207109455
vmkmanokumar <intern1@veyyil.com>
2025-01-31 17:15:07 +0530

    Scope Factors added

--- src/app/Scopes/ScopeOne/ScopeOneSelection.js
+++ src/app/Scopes/ScopeOne/ScopeOneSelection.js
@@ -1,25 +1,28 @@
 import { Flex } from "antd";
-import {CheckboxGroup, Checkbox} from "@heroui/checkbox";
+import { Checkbox } from "antd";
+import "./ScopeOneSelection.css";
+
 export default function ScopeOneSelection() {
+  const onChange = (checkedValues) => {
+    console.log("checked = ", checkedValues);
+  };
+
   return (
-    <>
-      <Flex
-        justify="center" // Horizontally center
-        align="center"   // Vertically center
-        className="mt-24 ml-[320px] h-60 w-60 bg-[#effbf7] rounded-lg"
-        style={{ height: "60vh",  width:"130vh"}} // Full viewport height
-      >
-         <CheckboxGroup defaultValue={["buenos-aires", "london"]} label="Select cities">
-      <Checkbox value="buenos-aires">Buenos Aires</Checkbox>
-      <Checkbox value="sydney">Sydney</Checkbox>
-      <Checkbox value="san-francisco">San Francisco</Checkbox>
-      <Checkbox value="london">London</Checkbox>
-      <Checkbox value="tokyo">Tokyo</Checkbox>
-    </CheckboxGroup>
+    <div className="flex justify-center items-center bg-[#effbf7] w-full md:w-[768px] lg:w-[1152px] md:mx-auto h-auto md:h-[450px] lg:h-[512px] mt-10 md:mt-16 lg:mt-24 p-4 md:p-6 rounded-xl">
+      <div className="w-full md:w-[500px] max-w-2xl rounded-lg p-4 md:p-6 lg:mr-96">
+        <h1 className="text-[20px] md:text-[22px] lg:text-[23px] font-black ">Scope Factors</h1>
 
-
-
-      </Flex>
-    </>
+        <Checkbox.Group onChange={onChange}>
+          <div className="flex flex-col gap-4 md:gap-5 lg:gap-6 mt-4 md:mt-6 lg:mt-8 text-[18px] md:text-[20px] lg:text-[23px]">
+            <Checkbox className="text-[18px] md:text-[20px] lg:text-[23px]" value="Fuels">Fuels</Checkbox>
+            <Checkbox className="text-[18px] md:text-[20px] lg:text-[23px]" value="Bioenergy">Bioenergy</Checkbox>
+            <Checkbox className="text-[18px] md:text-[20px] lg:text-[23px]" value="Refrigerant and others">Refrigerant and others</Checkbox>
+            <Checkbox className="text-[18px] md:text-[20px] lg:text-[23px]" value="Passenger vehicles">Passenger vehicles</Checkbox>
+            <Checkbox className="text-[18px] md:text-[20px] lg:text-[23px]" value="Delivery vehicles">Delivery vehicles</Checkbox>
+            <Checkbox className="text-[18px] md:text-[20px] lg:text-[23px]" value="SEWR KWh pass and delivery vehicles">SEWR KWh pass and delivery vehicles</Checkbox>
+          </div>
+        </Checkbox.Group>
+      </div>
+    </div>
   );
 }
