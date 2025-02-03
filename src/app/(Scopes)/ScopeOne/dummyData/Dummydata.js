da90d59de93a3e7bc5174cf86939f6e5dfb69f78
vmkmanokumar <intern1@veyyil.com>
2025-02-03 21:04:31 +0530

    small update

--- src/app/(Scopes)/ScopeOne/dummyData/Dummydata.js
+++ src/app/(Scopes)/ScopeOne/dummyData/Dummydata.js
@@ -216,7 +216,7 @@ export  const biogasDummyData = [
         values: ["tonnes", "kWh"],
       },
       {
-        name: "Biogas",
+        name: "BioGas",
         values: ["tonnes", "kWh"],
       },
       {
